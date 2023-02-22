207d971fca36c63afde3bc8d537a4d91b54650f5
Yoricom <64716261+yorinet@users.noreply.github.com>
2023-02-22 16:34:25 +0100

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,25 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const  productController = require('../controllers/products.controller');
-const  categoryController = require('../controllers/category.controller');
-const  inventaireController = require('../controllers/inventaire.controller');
+const  clientController = require('../controllers/clients.controller');
 
-router.get('/product', productController.getAll);
-router.post('/product', productController.create);
-router.get('/product/:id', productController.getById);
-router.put('/product/:id', productController.update);
-router.delete('/product/:id', productController.delete);
-
-router.get('/category', categoryController.getAll);
-router.post('/category', categoryController.create);
-router.get('/category/:id', categoryController.getById);
-router.put('/category/:id', categoryController.update);
-router.delete('/category/:id', categoryController.delete);
-
-router.get('/inventaire', inventaireController.getAll);
-router.post('/inventaire', inventaireController.create);
-router.get('/inventaire/:id', inventaireController.getById);
-router.put('/inventaire/:id', inventaireController.update);
-router.delete('/inventaire/:id', inventaireController.delete);
+router.get('/client', clientController.getAll);
+router.post('/client', clientController.create);
+router.get('/client/:id', clientController.getById);
+router.put('/client/:id', clientController.update);
+router.delete('/client/:id', clientController.delete);
 
 module.exports = router;
